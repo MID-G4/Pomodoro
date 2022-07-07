b3d65e98cb7eb0945e62f719d366a94aaeee9fc0
muhammadqasemtarboush1 <muhammadtarboush1@gmail.com>
2022-07-07 14:25:32 +0300

    init: add file basic structure

---
+++ pomodoro/main.py
@@ -0,0 +1,63 @@
+class ErrorHandler:
+    '''
+    To handle all errors in the whole app
+    '''
+
+    def __init__(self):
+        pass
+
+
+class Body:
+    '''
+    Implementing all the code functionally in it.
+    '''
+    def __init__(self):
+        pass
+
+    def welcoming(self):
+        pass
+
+    def timer(self):
+        '''
+        start the timer and break as SHB or LB
+        '''
+        pass
+
+    def options(self):
+        '''
+        display the options in each page instructions
+        '''
+        pass
+
+    def settings(self):
+        pass
+
+    def quit_app(self):
+        pass
+
+
+
+class Task:
+    def __init__(self):
+        pass
+
+    def add_task(self, task_title):
+        pass
+
+    def update_task(self, task):
+        pass
+
+    def list_tasks(self):
+        pass
+
+
+
+class Display:
+    '''
+    To render all the app content
+    functionally : root
+    '''
+
+    def __init__(self):
+        pass
+
